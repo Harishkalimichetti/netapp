e745f452186936376f235e0044e7839a2756d357
Harishkalimichetti <98823890+Harishkalimichetti@users.noreply.github.com>
2023-07-04 18:21:24 +0530

    Update na_ontap_file.py

--- ansible_collections/netapp/ontap/plugins/modules/na_ontap_file.py
+++ ansible_collections/netapp/ontap/plugins/modules/na_ontap_file.py
@@ -214,7 +214,7 @@ class NetAppOntapFilePolicy(object):
       run_command.add_child_elem(args)
 
       try:
-          result:self.server.invoke_seccessfully(run_command, enable_tunneling=False)
+          result:self.server.invoke_successfully(run_command, enable_tunneling=False)
 
       except netapp_utils.zapi.NaApiError:
           err = get_exception()
@@ -231,188 +231,81 @@ class NetAppOntapFilePolicy(object):
       command='extract_and_chown'
       if self.starup == "":
          command='chown_only'
-                                 
-        
+
+      chown_info['command']=command
+                
+      run_command = netapp_utils.zapi.NaElement('system-cli')
+      args = netapp_utils.zapi.NaElement('args')
+      args.add_new_child('arg','set')
+      args.add_new_child('arg','-priv')
+      args.add_new_child('arg','diag')
+      args.add_new_child('arg',';')
+      args.add_new_child('arg','system')
+      args.add_new_child('arg','node')
+      args.add_new_child('arg','systemshell')
+      args.add_new_child('arg','-node')
+      args.add_new_child('arg',vserver_root_volume_node)
+      args.add_new_child('arg','-command')
+      args.add_new_child('arg','sudo')
+      args.add_new_child('arg','/clus/'+self.vserver.path+'/chownhome.sh')
+      args.add_new_child('arg','command')
+      args.add_new_child('arg',self.uid)
+      args.add_new_child('arg',self.gid)          
+      run_command.add_child_elem(args)
+
+      try:
+          result:self.server.invoke_successfully(run_command, enable_tunneling=False)
+
+      except netapp_utils.zapi.NaApiError:
+          err = get_exception()
+          self.module.fail_json(msg='error running api for node %s option %s' % (self.node,self.option),exception=str(err))
+          return None
+
+      cli_output=result.get_child_content('cli-output')
+      cli_result=result.get_child_content('cki-result-value')
+
+      chown_info['chownhome_sh_result']=cli_result
+      chown_info['chownhome_sh_output']=cli_output
+
+      # self.module.fail_json(msg='Ran command with result %s and output %s' %(cli_result, cli_output), exception='WARNING')
+
+      return chown_info
 
 
+def copy_file(self,curfile):
+
+  with open(curfile, 'rb') as f:
+    hex_content = f.read().encode('hex')
+    #print hex_content
+    pathinfo=self.path.split('/')
+    write_file_path='/vol/'+pathinfo[2]+'/'+pathinfo[3]+'/'+curfile.split('/')[-1]
+    #self.module.fail_json(msg="write_file_path = %s" % (write_file_path), exception='WARNING')
+
+    file_create = netapp_utils.zapi.NaElement.create_node_with_children(
+      'file-write-file', **{'data': hex_content,
+                            'offset': '0',
+                            'path': write_file_path})
+
+    try:
+       self.server.invoke_successfully(file_create, enable_tunneling=True)
+
+    except netapp_utils.zapi.NaApiError as e:
+       self.module.fail_json(msg="Error writing file %s on vserver %s" % (write_fie_path, self.vserver, to_native(e)),
+                             exception=traceback.format_exc())
 
 
-def create_policy(self):
-        policy_obj = netapp_utils.zapi.NaElement("file-directory-security-policy-create")
-        policy_obj.add_new_child('policy-name', self.parameters['policy_name'])
-        try:
-            self.server.invoke_successfully(policy_obj, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(
-                msg='Error creating file-directory policy %s: %s' % (self.parameters['policy_name'], to_native(error)),
-                exception=traceback.format_exc())
-
-    def get_policy_iter(self):
-        policy_get_iter = netapp_utils.zapi.NaElement('file-directory-security-policy-get-iter')
-        policy_info = netapp_utils.zapi.NaElement('file-directory-security-policy')
-        policy_info.add_new_child('vserver', self.parameters['vserver'])
-        policy_info.add_new_child('policy-name', self.parameters['policy_name'])
-        query = netapp_utils.zapi.NaElement('query')
-        query.add_child_elem(policy_info)
-        policy_get_iter.add_child_elem(query)
-        try:
-            result = self.server.invoke_successfully(policy_get_iter, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error fetching file-directory policy %s: %s'
-                                      % (self.parameters['policy_name'], to_native(error)),
-                                  exception=traceback.format_exc())
-        if result.get_child_by_name('num-records') and int(result.get_child_content('num-records')) >= 1:
-            attributes_list = result.get_child_by_name('attributes-list')
-            policy = attributes_list.get_child_by_name('file-directory-security-policy')
-            return policy.get_child_content('policy-name')
-        return None
-
-    def remove_policy(self):
-        remove_policy = netapp_utils.zapi.NaElement('file-directory-security-policy-delete')
-        remove_policy.add_new_child('policy-name', self.parameters['policy_name'])
-        try:
-            self.server.invoke_successfully(remove_policy, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(
-                msg='Error removing file-directory policy %s: %s' % (self.parameters['policy_name'], to_native(error)),
-                exception=traceback.format_exc())
-
-    def get_task_iter(self):
-        task_get_iter = netapp_utils.zapi.NaElement('file-directory-security-policy-task-get-iter')
-        task_info = netapp_utils.zapi.NaElement('file-directory-security-policy-task')
-        task_info.add_new_child('vserver', self.parameters['vserver'])
-        task_info.add_new_child('policy-name', self.parameters['policy_name'])
-        task_info.add_new_child('path', self.parameters['path'])
-        query = netapp_utils.zapi.NaElement('query')
-        query.add_child_elem(task_info)
-        task_get_iter.add_child_elem(query)
-        try:
-            result = self.server.invoke_successfully(task_get_iter, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error fetching task from file-directory policy %s: %s'
-                                      % (self.parameters['policy_name'], to_native(error)),
-                                  exception=traceback.format_exc())
-        if result.get_child_by_name('num-records') and int(result.get_child_content('num-records')) >= 1:
-            attributes_list = result.get_child_by_name('attributes-list')
-            task = attributes_list.get_child_by_name('file-directory-security-policy-task')
-            task_result = dict()
-            task_result['path'] = task.get_child_content('path')
-            if task.get_child_by_name('ntfs-mode'):
-                task_result['ntfs_mode'] = task.get_child_content('ntfs-mode')
-            if task.get_child_by_name('security-type'):
-                task_result['security_type'] = task.get_child_content('security-type')
-            if task.get_child_by_name('ntfs-sd'):
-                task_result['ntfs_sd'] = [ntfs_sd.get_content() for ntfs_sd in task.get_child_by_name('ntfs-sd').get_children()]
-            return task_result
-        return None
-
-    def add_task_to_policy(self):
-        policy_add_task = netapp_utils.zapi.NaElement('file-directory-security-policy-task-add')
-        policy_add_task.add_new_child('path', self.parameters['path'])
-        policy_add_task.add_new_child('policy-name', self.parameters['policy_name'])
-        if self.parameters.get('access_control') is not None:
-            policy_add_task.add_new_child('access-control', self.parameters['access_control'])
-        if self.parameters.get('ntfs_mode') is not None:
-            policy_add_task.add_new_child('ntfs-mode', self.parameters['ntfs_mode'])
-        if self.parameters.get('ntfs_sd') is not None:
-            ntfs_sds = netapp_utils.zapi.NaElement('ntfs-sd')
-            for ntfs_sd in self.parameters['ntfs_sd']:
-                ntfs_sds.add_new_child('file-security-ntfs-sd', ntfs_sd)
-            policy_add_task.add_child_elem(ntfs_sds)
-        if self.parameters.get('security_type') is not None:
-            policy_add_task.add_new_child('security-type', self.parameters['security_type'])
-        try:
-            self.server.invoke_successfully(policy_add_task, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error adding task to file-directory policy %s: %s'
-                                      % (self.parameters['policy_name'], to_native(error)),
-                                  exception=traceback.format_exc())
-
-    def remove_task_from_policy(self):
-        policy_remove_task = netapp_utils.zapi.NaElement('file-directory-security-policy-task-remove')
-        policy_remove_task.add_new_child('path', self.parameters['path'])
-        policy_remove_task.add_new_child('policy-name', self.parameters['policy_name'])
-        try:
-            self.server.invoke_successfully(policy_remove_task, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error removing task from file-directory policy %s: %s'
-                                      % (self.parameters['policy_name'], to_native(error)),
-                                  exception=traceback.format_exc())
-
-    def modify_task(self, modify):
-        policy_modify_task = netapp_utils.zapi.NaElement('file-directory-security-policy-task-modify')
-        policy_modify_task.add_new_child('path', self.parameters['path'])
-        policy_modify_task.add_new_child('policy-name', self.parameters['policy_name'])
-        if modify.get('ntfs_mode') is not None:
-            policy_modify_task.add_new_child('ntfs-mode', self.parameters['ntfs_mode'])
-        if modify.get('ntfs_sd') is not None:
-            ntfs_sds = netapp_utils.zapi.NaElement('ntfs-sd')
-            for ntfs_sd in self.parameters['ntfs_sd']:
-                ntfs_sds.add_new_child('file-security-ntfs-sd', ntfs_sd)
-            policy_modify_task.add_child_elem(ntfs_sds)
-        if modify.get('security_type') is not None:
-            policy_modify_task.add_new_child('security-type', self.parameters['security_type'])
-        try:
-            self.server.invoke_successfully(policy_modify_task, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error modifying task in file-directory policy %s: %s'
-                                      % (self.parameters['policy_name'], to_native(error)),
-                                  exception=traceback.format_exc())
-
-    def set_sd(self):
-        set_sd = netapp_utils.zapi.NaElement('file-directory-security-set')
-        set_sd.add_new_child('policy-name', self.parameters['policy_name'])
-        if self.parameters.get('ignore-broken-symlinks'):
-            set_sd.add_new_child('ignore-broken-symlinks', str(self.parameters['ignore_broken_symlinks']))
-        try:
-            self.server.invoke_successfully(set_sd, True)
-        except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error applying file-directory policy %s: %s'
-                                      % (self.parameters['policy_name'], to_native(error)),
-                                  exception=traceback.format_exc())
-
-    def apply(self):
-        netapp_utils.ems_log_event("na_ontap_file_directory_policy", self.server)
-        current = self.get_policy_iter()
-        cd_action, task_cd_action, task_modify = None, None, None
-        cd_action = self.na_helper.get_cd_action(current, self.parameters)
-        if self.parameters.get('path'):
-            current_task = self.get_task_iter()
-            task_cd_action = self.na_helper.get_cd_action(current_task, self.parameters)
-            if task_cd_action is None and self.parameters['state'] == 'present':
-                task_modify = self.na_helper.get_modified_attributes(current_task, self.parameters)
-        if self.na_helper.changed:
-            if self.module.check_mode:
-                pass
-            else:
-                if self.parameters.get('path'):
-                    if task_cd_action == 'create':
-                        # if policy doesn't exist, create the policy first.
-                        if cd_action == 'create':
-                            self.create_policy()
-                        self.add_task_to_policy()
-                        self.set_sd()
-                    elif task_cd_action == 'delete':
-                        # delete the task, not the policy.
-                        self.remove_task_from_policy()
-                    elif task_modify:
-                        self.modify_task(task_modify)
-                        self.set_sd()
-                else:
-                    if cd_action == 'create':
-                        self.create_policy()
-                        self.set_sd()
-                    elif cd_action == 'delete':
-                        self.remove_policy()
-        self.module.exit_json(changed=self.na_helper.changed)
+def apply(Self):
+    changed: False
+    if self.startup != "":
+      self.copy_file(self.startup)
+    self.copy_file(self.chownhome)
+    chownhome_output=self.run_chownhome()
+    self.module.exit_json(changed=changed,output=chown_output)
 
 
 def main():
-    """
-    Creates, deletes and modifies file directory policy
-    """
-    obj = NetAppOntapFilePolicy()
-    obj.apply()
+    v = NetAppOntapQTree()
+    v.apply()
 
 
 if __name__ == '__main__':
