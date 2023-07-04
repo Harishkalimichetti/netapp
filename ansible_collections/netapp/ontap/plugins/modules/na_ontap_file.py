dda3121ac3360acef5a6a290f81da05943912882
Harishkalimichetti <98823890+Harishkalimichetti@users.noreply.github.com>
2023-07-04 17:57:54 +0530

    Update na_ontap_file.py

--- ansible_collections/netapp/ontap/plugins/modules/na_ontap_file.py
+++ ansible_collections/netapp/ontap/plugins/modules/na_ontap_file.py
@@ -113,16 +113,131 @@ class NetAppOntapFilePolicy(object):
 
         p = self.module.params
 
-        # set up variables
-        self.na_helper = NetAppModule()
-        self.parameters = self.na_helper.set_parameters(self.module.params)
+        # set up state variables
+        self.state = p['state']
+        self.path = p['path']
+        self.chownhome = p['chownhome']
+        self.startup = p['startup']
+        self.vserver = p['vserver']
+        self.uid = p['uid']
+        self.gid = p['gid']
 
         if HAS_NETAPP_LIB is False:
-            self.module.fail_json(msg='The python NetApp-Lib module is required')
+            self.module.fail_json(
+                msg='The python NetApp-Lib module is required')
         else:
-            self.server = netapp_utils.setup_na_ontap_zapi(module=self.module, vserver=self.parameters['vserver'])
+            self.server = netapp_utils.setup_na_ontap_zapi(
+                module=self.module, vserver=self.parameters['vserver'])
 
-    def create_policy(self):
+  def run_chownhome(self):
+
+      chown_info={}
+
+      volume_name=self.path.split('/')[2]
+      chown_info['volume_name']=volume_name
+      volume_info = netapp_utils.zapi.NaElement('volume-get-iter')
+      volume_attributes = netapp_utils.zapi.NaElement('volume-attribute')
+      volume_id_attributes = netapp_utils.zapi.NaElement('volume-id-attribute')
+      volume_id_attributes.add_new_child('name', volume_name)
+      volume_id_attributes.add_new_child('owning-vserver-name', self.vserver)
+      volume_attributes.add_child_elem(volume_id_attributes)
+      query = netapp_utils.zapi.NaElement('query')
+      query.add_child_elem(volume_attributes)
+      volume_info.add_child_elem(query)
+      try:
+          result = self.server.invoke_successfully(volume_info, True)
+      except netapp_utils.zapi.NaApiError as error:
+          self.module.fail_json(msg='Error fetching volume %s : %s' % (volume_name, to_native(error)), exception=traceback.
+      volume_node="UNKNOWN"
+      if result.get_child_by_name('num-records') and \
+              int(result.get_child_content('num-records')) > 0:
+
+         volume_attributes = result.get_child_by_name( 'attributes-list').get_child_by_name( 'volume-attributes')
+         volume_id_attributes = volume_attributes.get_child_by_name( 'volume-id-attributes')
+         volume_node = volume_id_attributes.get_child_content('node')
+         chown_info['volume_node']=volume_node
+         #self.module.fail_json(msg='Found volume %s on node %s' % (volume_name, volume_node), exception=traceback.format_exc())
+      else:
+         self.module.fail_json(msg='Volume attributes for %s on vserver %s could not be found' % (volume_name, self.vserver), exception='WARNING')
+
+
+      chown_info['vserver']=self.vserver
+      volume_info = netapp_utils.zapi.NaElement('volume-get-iter')
+      volume_attributes = netapp_utils.zapi.NaElement(volume-attributes')
+
+      volume_id_attributes = netapp_utils.zapi.NaElement('volume-id-attributes')
+      volume_id_attributes.add_new_child('owning-vserver-name', self.vserver)
+      volume_id_attributes.add_new_child('type', 'rw')
+      volume_attributes.add_child_elem(volume_id_attributes)
+
+      volume_state_attributes = netapp_utils.zapi.NaElement('volume-state-attributes')
+      volume_state_attributes.add_new_child('is-vserver-root', 'true')
+      volume_attributes.add_child_elem(volume_state_attributes)
+
+      query = netapp_utils.zapi.NaElement('query')
+      query.add_child_elem(volume_attributes)
+      volume_info.add_child_elem(query)
+      try:
+           result = self.server.invoke_successfully(volume_info, True)
+      except netapp_utils.zapi.NaApiError as error:
+           self.module.fail_json(msg='Error fetching volume %s : %s' % (volume_name, to_native(error)), exception=traceback.format_exc())
+      vserver_root_volume_node="UNKNOWN"
+      if result.get_child_by_name('num-records') and \
+              int(result.get_child_content('num-records')) > 0:
+
+         volume_attributes = result.get_child_by_name( 'attributes-list').get_child_by_name( 'volume-attributes')
+         volume_id_attributes = volume_attributes.get_child_by_name( 'volume-id-attributes')
+         vserver_root_volume_node=volume_it_attributes.get_child_content('node')
+         chown_info['vserver_root_volume_node']=vserver_root_volume_node
+         #self.module.fail_json(msg='Found volume %s on node %s' % (volume_name, volume_node), exception=traceback.format_exc())
+      else:
+         self.module.fail_json(msg='Volume attributes for %s on vserver %s could not be found' % (volume_name, self.vserver), exception='WARNING')
+
+      chown_script='/clus/'+self.vserver+self.path+'/chownhome.sh'
+      chown_info['chown_script']=chown_script
+      run_command = netapp_utils.zapi.NaElement('system-cli')
+      args = netapp_utils.zapi.NaElement('args')
+      args.add_new_child('arg','set')
+      args.add_new_child('arg','-priv')
+      args.add_new_child('arg','diag')
+      args.add_new_child('arg',';')
+      args.add_new_child('arg','system')
+      args.add_new_child('arg','node')
+      args.add_new_child('arg','systemshell')
+      args.add_new_child('arg','-node')
+      args.add_new_child('arg',vserver_root_volume_node)
+      args.add_new_child('arg','-command')
+      args.add_new_child('arg','sudo')
+      args.add_new_child('arg','chmod')
+      args.add_new_child('arg','755')
+      args.add_new_child('arg',chown_script)
+      run_command.add_child_elem(args)
+
+      try:
+          result:self.server.invoke_seccessfully(run_command, enable_tunneling=False)
+
+      except netapp_utils.zapi.NaApiError:
+          err = get_exception()
+          self.module.fail_json(msg='Error running chown 755 command for %s' % (chown_script),exception=str(err))
+          return None
+
+      cli_output=result.get_child_content('cli-output')
+      cli_result=result.get_child_content('cki-result-value')
+      chown_info['chmod_755_result']=cli_result
+      chown_info['chmod_755_output']=cli_output
+
+      # self.module.fail_json(msg='Ran command with result %s and output %s' %(cli_result, cli_output), exception='WARNING')
+
+      command='extract_and_chown'
+      if self.starup == "":
+         command='chown_only'
+                                 
+        
+
+
+
+
+def create_policy(self):
         policy_obj = netapp_utils.zapi.NaElement("file-directory-security-policy-create")
         policy_obj.add_new_child('policy-name', self.parameters['policy_name'])
         try:
